04335c5b226e63cbf97a59dfd4663fb64512980f
d4bes2000 <d4bes2000@gmail.com>
2018-02-17 23:34:01 -0600

    Add files via upload

---
+++ adventure_rpg/Maps/Tilesets/Rogue-like.tsx
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Rogue-like" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="roguelikeSheet_magenta.png" trans="000000" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dark Grass" tile="899"/>
+  <terrain name="Dirt" tile="568"/>
+  <terrain name="Sand" tile="1240"/>
+  <terrain name="Stone" tile="904"/>
+ </terraintypes>
+ <tile id="518" terrain="1,1,1,"/>
+ <tile id="519" terrain="1,1,,1"/>
+ <tile id="520" terrain=",,,1"/>
+ <tile id="521" terrain=",,1,1"/>
+ <tile id="522" terrain=",,1,"/>
+ <tile id="575" terrain="1,,1,1"/>
+ <tile id="576" terrain=",1,1,1"/>
+ <tile id="577" terrain=",1,,1"/>
+ <tile id="578" terrain="1,1,1,1"/>
+ <tile id="579" terrain="1,,1,"/>
+ <tile id="634" terrain=",1,,"/>
+ <tile id="635" terrain="1,1,,"/>
+ <tile id="636" terrain="1,,,"/>
+ <tile id="855" terrain="0,0,0,"/>
+ <tile id="856" terrain="0,0,,0"/>
+ <tile id="857" terrain=",,,0"/>
+ <tile id="858" terrain=",,0,0"/>
+ <tile id="859" terrain=",,0,"/>
+ <tile id="860" terrain="3,3,3,"/>
+ <tile id="861" terrain="3,3,,3"/>
+ <tile id="862" terrain=",,,3"/>
+ <tile id="863" terrain=",,3,3"/>
+ <tile id="864" terrain=",,3,"/>
+ <tile id="912" terrain="0,,0,0"/>
+ <tile id="913" terrain=",0,0,0"/>
+ <tile id="914" terrain=",0,,0"/>
+ <tile id="915" terrain="0,0,0,0"/>
+ <tile id="916" terrain="0,,0,"/>
+ <tile id="917" terrain="3,,3,3"/>
+ <tile id="918" terrain=",3,3,3"/>
+ <tile id="919" terrain=",3,,3"/>
+ <tile id="920" terrain="3,3,3,3"/>
+ <tile id="921" terrain="3,,3,"/>
+ <tile id="971" terrain=",0,,"/>
+ <tile id="972" terrain="0,0,,"/>
+ <tile id="973" terrain="0,,,"/>
+ <tile id="976" terrain=",3,,"/>
+ <tile id="977" terrain="3,3,,"/>
+ <tile id="978" terrain="3,,,"/>
+ <tile id="1202" terrain="2,2,2,"/>
+ <tile id="1203" terrain="2,2,,2"/>
+ <tile id="1204" terrain=",,,2"/>
+ <tile id="1205" terrain=",,2,2"/>
+ <tile id="1206" terrain=",,2,"/>
+ <tile id="1259" terrain="2,,2,2"/>
+ <tile id="1260" terrain=",2,2,2"/>
+ <tile id="1261" terrain=",2,,2"/>
+ <tile id="1262" terrain="2,2,2,2"/>
+ <tile id="1263" terrain="2,,2,"/>
+ <tile id="1318" terrain=",2,,"/>
+ <tile id="1319" terrain="2,2,,"/>
+ <tile id="1320" terrain="2,,,"/>
+</tileset>
---
+++ adventure_rpg/Sprites/Character_Animations/Walking Spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Walking Spritesheet" tilewidth="16" tileheight="24" tilecount="16" columns="4">
+ <image source="Walking Spritesheet.bmp" trans="ffffff" width="64" height="96"/>
+</tileset>
